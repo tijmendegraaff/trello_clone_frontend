5952b977ce16fe16ca1ab4fa9c2fdacbb0ee43e1
Tijmen de Graaff <tijmen76@gmail.com>
2019-04-18 21:15:58 +0200

    setup from on login and register page

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,5 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
-const LoginPage = () => <div>This is from my LoginPage!</div>;
+class LoginPage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: '',
+      password: '',
+      submitted: false,
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  }
+
+  render() {
+    console.log(this.state);
+    const { username, password } = this.state;
+    return (
+      <div>
+        <div>This is from my LoginPage!</div>
+        <form name="form" onSubmit={this.handleSubmit}>
+          <div>
+            <div>Email</div>
+            <input type="text" name="username" value={username} onChange={this.handleChange} />
+          </div>
+          <div>
+            <div>Password</div>
+            <input type="password" name="password" value={password} onChange={this.handleChange} />
+          </div>
+          <div>
+            <button type="submit">Login</button>
+          </div>
+        </form>
+        <Link to="/register">Register</Link>
+      </div>
+    );
+  }
+}
 
 export default LoginPage;
--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,5 +1,73 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
-const RegisterPage = () => <div>This is from my RegisterPage!</div>;
+class RegisterPage extends Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      user: {
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: '',
+      },
+      submitted: false,
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    const { name, value } = event.target;
+    const { user } = this.state;
+    this.setState({
+      user: {
+        ...user,
+        [name]: value,
+      },
+    });
+  }
+
+  render() {
+    console.log(this.state);
+    const { user } = this.state;
+    return (
+      <div>
+        <div>This is from my RegisterPage!</div>
+        <form name="form" onSubmit={this.handleSubmit}>
+          <div>
+            <div>First Name</div>
+            <input
+              type="text"
+              name="firstName"
+              value={user.firstName}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div>
+            <div>Last Name</div>
+            <input type="text" name="lastName" value={user.lastName} onChange={this.handleChange} />
+          </div>
+          <div>
+            <div>email</div>
+            <input type="email" name="email" value={user.email} onChange={this.handleChange} />
+          </div>
+          <div>
+            <div>Password</div>
+            <input
+              type="password"
+              name="password"
+              value={user.password}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div className="form-group">
+            <button type="submit">Register</button>
+          </div>
+        </form>
+        <Link to="/login">Login</Link>
+      </div>
+    );
+  }
+}
 export default RegisterPage;
