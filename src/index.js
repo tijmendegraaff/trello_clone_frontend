4850462e4166779eca057a6bc24ebcb478f9648e
Tijmen de Graaff <tijmen76@gmail.com>
2019-04-18 20:31:50 +0200

    add Provider to app

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import configureStore from './store/configureStore';
 import AppRouter from './routers/AppRouter';
 
 import './styles/styles.scss';
 
-render(<AppRouter />, document.getElementById('app'));
+const store = configureStore();
+
+const App = (
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
+);
+render(App, document.getElementById('app'));
