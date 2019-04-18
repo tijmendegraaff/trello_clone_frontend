8308144f96b5f00d0e1c8e8d4af6de96f32f3d25
Tijmen de Graaff <tijmen76@gmail.com>
2019-04-18 18:50:43 +0200

    setup scss & stylesheet structure

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { render } from "react-dom";
 
+import "./styles/styles.scss";
+
 const App = () => <div>Hello world</div>;
 
 render(<App />, document.getElementById("app"));
