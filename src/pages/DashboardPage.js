17a35fc733f8f6f1b88204fc272a5d27a45110c4
Tijmen de Graaff <tijmen76@gmail.com>
2019-04-19 07:43:25 +0200

    connect DashboardPage to redux

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -1,5 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
-const DashboardPage = () => <div>This is from my dashboard component!</div>;
+class DashboardPage extends Component {
+  constructor(props) {
+    super(props);
 
-export default DashboardPage;
+    this.state = {
+      loading: true,
+    };
+  }
+
+  async componentDidMount() {
+    const { fetchBoards, fetchUser } = this.props;
+    await fetchBoards();
+    await fetchUser();
+    this.setState({ loading: false });
+  }
+
+  render() {
+    const { loading } = this.state;
+    const { currentUser, boards } = this.props;
+
+    const board = ({ _id, name }) => (
+      <Link key={`board-${_id}`} to={`/boards/${_id}`}>
+        <li>{name}</li>
+      </Link>
+    );
+    return loading ? (
+      <p>loading</p>
+    ) : (
+      <div>
+        <h1>
+          Welkom back
+          {currentUser.firstName}
+        </h1>
+        This is from my dashboard component!
+        <div>
+          {boards.length !== 0 ? boards.map(b => board(b)) : null}
+          <Link to="/boards/create">create a board</Link>
+        </div>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps({ boards, currentUser }) {
+  return { boards, currentUser };
+}
+export default connect(
+  mapStateToProps,
+  actions,
+)(DashboardPage);
--- src/reducers/auth.js
+++
@@ -1,2 +0,0 @@
-const user = JSON.parse(localStorage.getItem('user'));
-const initialState = user ? { loggedIn: true, user } : {};
