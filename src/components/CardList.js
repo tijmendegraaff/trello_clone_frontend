938da5bb8e76eb40835c160ebd35c2d89308a431
Tijmen de Graaff <tijmen76@gmail.com>
2019-04-19 07:45:15 +0200

    add cardlist component

---
+++ src/components/CardList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const CardList = ({ title }) => (
+  <div>
+    List
+    {title}
+  </div>
+);
+
+export default CardList;
--- src/pages/BoardPage.js
+++ src/pages/BoardPage.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
+import CardList from '../components/CardList';
+
 const BoardPage = ({ match }) => (
   <div>
+    <h1>
       This is from my Board page with the id of
-    {match.params.id}
+      {match.params.id}
+    </h1>
+    <CardList />
   </div>
 );
 
