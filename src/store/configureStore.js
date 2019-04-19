1327f9fcbbd067dce927c0a809fae5bb6a711fda
Tijmen de Graaff <tijmen76@gmail.com>
2019-04-19 07:43:40 +0200

    import reducers

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,14 +2,14 @@ import {
   createStore, combineReducers, applyMiddleware, compose,
 } from 'redux';
 import thunk from 'redux-thunk';
-import authReducer from '../reducers/authReducer';
+import currentUserReducer from '../reducers/currentUserReducer';
 import boardReducer from '../reducers/boardReducer';
 
 // eslint-disable-next-line no-underscore-dangle
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 export default () => {
   const store = createStore(
-    combineReducers({ auth: authReducer, boards: boardReducer }),
+    combineReducers({ currentUser: currentUserReducer, boards: boardReducer }),
     composeEnhancers(applyMiddleware(thunk)),
   );
 
