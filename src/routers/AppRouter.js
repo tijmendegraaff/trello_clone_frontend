bdaf6a925e9be1d2466657a9a3cd09c3af816029
Tijmen de Graaff <tijmen76@gmail.com>
2019-04-19 07:17:57 +0200

    check for valid token or redirect

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,21 +1,49 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import {
+  BrowserRouter, Route, Switch, Redirect,
+} from 'react-router-dom';
+import jwtDecode from 'jwt-decode';
 import DashboardPage from '../pages/DashboardPage';
 import BoardPage from '../pages/BoardPage';
 import LoginPage from '../pages/LoginPage';
 import RegisterPage from '../pages/RegisterPage';
 import NotFoundPage from '../pages/NotFoundPage';
 
-const AppRouter = () => (
+const isAuthenticated = () => {
+  const token = localStorage.getItem('token');
+  try {
+    jwtDecode(token);
+  } catch (err) {
+    return false;
+  }
+  return true;
+};
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props => (isAuthenticated() ? (
+      <Component {...props} />
+    ) : (
+      <Redirect
+        to={{
+          pathname: '/login',
+          state: { from: props.location },
+        }}
+      />
+    ))
+    }
+  />
+);
+
+export default () => (
   <BrowserRouter>
     <Switch>
-      <Route path="/" component={DashboardPage} exact />
-      <Route path="/boards/:id" component={BoardPage} />
-      <Route path="/login" component={LoginPage} />
-      <Route path="/register" component={RegisterPage} />
+      <Route path="/login" exact component={LoginPage} />
+      <Route path="/register" exact component={RegisterPage} />
+      <PrivateRoute path="/boards" exact component={DashboardPage} />
+      <PrivateRoute path="/boards/:id" exact component={BoardPage} />
       <Route component={NotFoundPage} />
     </Switch>
   </BrowserRouter>
 );
-
-export default AppRouter;
