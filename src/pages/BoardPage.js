aaeb33be83f16dfd11cadd1f3bb7d18dde8fd046
Tijmen de Graaff <tijmen76@gmail.com>
2019-04-19 10:46:35 +0200

    connect board page to redux

--- src/pages/BoardPage.js
+++ src/pages/BoardPage.js
@@ -1,15 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 import CardList from '../components/CardList';
 
-const BoardPage = ({ match }) => (
-  <div>
-    <h1>
-      This is from my Board page with the id of
-      {match.params.id}
-    </h1>
-    <CardList />
-  </div>
-);
+class BoardPage extends Component {
+  constructor(props) {
+    super(props);
 
-export default BoardPage;
+    this.state = {
+      loading: true,
+    };
+  }
+
+  async componentDidMount() {
+    const { fetchBoard } = this.props;
+    const { id } = this.props.match.params;
+    await fetchBoard(id);
+    this.setState({ loading: false });
+  }
+
+  render() {
+    console.log(this.props);
+    const { name } = this.props.board;
+    const { loading } = this.state;
+    return loading ? (
+      <p>loading</p>
+    ) : (
+      <div>
+        <h1>
+          This is the board with name:
+          {name}
+        </h1>
+        <CardList title="test" />
+      </div>
+    );
+  }
+}
+function mapStateToProps({ board }) {
+  return { board };
+}
+export default connect(
+  mapStateToProps,
+  actions,
+)(BoardPage);
